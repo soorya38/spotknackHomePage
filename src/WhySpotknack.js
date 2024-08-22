edab5f4ee27a641af88b90a49ef79194e2b91050
soorya38 <csasoorya@gmail.com>
2024-08-22 18:15:15 +0530

    added features

--- src/WhySpotknack.js
+++ src/WhySpotknack.js
@@ -1,8 +1,15 @@
 function WhySpotknack() {
     return (
         <div className='outer-container-features'>
+            <div className='inner-container-wspk'>
+
+            </div>
             <h2 className='hf-title' style={{padding: '20px'}}>Why Spotknack !</h2>
             <div className='left-side'>
+                <h1>Our mission is user Convenience</h1>
+                <h3>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequit</h3>
+            </div>
+            <div className='right-side'>
                 <h1>Our mission is user Convenience</h1>
                 <h3>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequit</h3>
             </div>
